53c3efe9994aa7060ab89469d900950dedbfc531
keymod <modi.ke@northeastern.edu>
2022-10-06 16:49:04 -0400

    Added Search function, validation & solved upload image issue

---
+++ src/model/EmployeeDetails.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+import java.util.Date;
+
+/**
+ *
+ * @author lemin
+ */
+public class EmployeeDetails {
+    
+    private String name;
+    private String employeeID;
+    private int age;
+    private String gender;
+    private Date startDate;
+    private String level;
+    private String teamInfo;
+    private String title;
+    private String number;
+    private String email;
+    private Image photo;
+    private String date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public Image getPhoto(){
+        return photo;
+    }
+    
+    public void setPhoto(Image Photo){
+        this.photo = Photo;
+    }
+   
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    public String setDate() {
+        return date;
+    }
+}
